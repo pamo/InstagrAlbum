38782d27aa0dc4478c0c93f24f27b16e092a3434
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-02-28 00:01:10 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instagralbum::Application.config.secret_token = '377611f44efe86e290983ad76c016b5506aa027fe84e85a97e1252af0303eab019b5a82f0d3daf56ca87b97f7382547ee685eb96f777e87dca3a3781d7aa96ca'
