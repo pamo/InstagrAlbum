4ca25ff5931b6edf547e73c7496ccce50fe9e8cf
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-03-04 11:09:26 -0500

    refactored album to albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Instagralbum::Application.routes.draw do
   resources :albums
 
-    get 'album/index'
+    get 'albums/index'
     match 'session/:action', :to => 'sessions'
   # The priority is based upon order of creation:
   # first created -> highest priority.
