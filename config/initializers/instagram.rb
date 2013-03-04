1181fa11e7b087e51f7b0d08e3caf5b316beb35f
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-03-03 23:40:56 -0500

    debugging production issues

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -3,6 +3,7 @@ require "instagram"
 Instagram.configure do |config|
     config_file = Rails.root.join 'config', 'api.yml'
     api = YAML.load_file(config_file)
+    puts api
     config.client_id = api['client_id']
     config.client_secret = api['client_secret']
 end
